10b4a241c1fcbb8edf4dbd73af276566b83ee0ea
wanghuajian620 <154659041@qq.com>
2017-10-28 20:39:51 +0800

    TaoBao   首页以及我的以及 跳转页面全部使用 列表渲染

--- TaoBao/src/parameter/content.js
+++ TaoBao/src/parameter/content.js
@@ -20,10 +20,64 @@ module.exports = [
       'https://gd4.alicdn.com/imgextra/i4/143440491/TB2RqFPeWmWQ1JjSZPhXXXCJFXa_!!143440491.jpg'
     ]
   }, {
-    icon: 'https://gd3.alicdn.com/imgextra/i3/67515073/TB15lr7bvNNTKJjSspcXXb4KVXa_!!0-item_pic.jpg',
-    title: '丢丢韩版原宿宽松绞花毛线针织无袖马甲背心外套马甲女 💰159'
+    icon: 'https://gd1.alicdn.com/imgextra/i2/2537026158/TB2SCG8lbsTMeJjy1zeXXcOCVXa_!!2537026158.jpg',
+    title: '复古流苏毛衫针织衫女秋冬新款黄色长袖高领慵懒风毛衣套头粗毛线 💰229',
+    pic: [
+      'https://gd1.alicdn.com/imgextra/i2/2537026158/TB2SCG8lbsTMeJjy1zeXXcOCVXa_!!2537026158.jpg',
+      'https://gd1.alicdn.com/imgextra/i1/2537026158/TB2cVByhjihSKJjy0FeXXbJtpXa_!!2537026158.jpg',
+      'https://gd1.alicdn.com/imgextra/i1/2537026158/TB22lR0hjqhSKJjSspnXXc79XXa_!!2537026158.jpg',
+      'https://gd2.alicdn.com/imgextra/i2/2537026158/TB2IHrXgvBNTKJjSszeXXcu2VXa_!!2537026158.jpg',
+      'https://gd3.alicdn.com/imgextra/i3/2537026158/TB2omxXXysF4uJjSZFtXXXHwVXa_!!2537026158.jpg'
+    ]
   }, {
-    icon: 'https://gd2.alicdn.com/imgextra/i2/2537026158/TB2Pcy9lbsTMeJjy1zeXXcOCVXa_!!2537026158.jpg',
-    title: '羊毛格子复古V领背心马甲女套头宽松针织毛线宽松韩版 💰229'
+    icon: 'https://gd3.alicdn.com/imgextra/i3/539549300/TB2wuJFfAWM.eBjSZFhXXbdWpXa_!!539549300.jpg',
+    title: '滕雨佳SHOCK AMIU冬季大毛领中长款羽绒服女新款韩国同款加厚外套 💰399',
+    pic: [
+      'https://gd3.alicdn.com/imgextra/i3/539549300/TB2wuJFfAWM.eBjSZFhXXbdWpXa_!!539549300.jpg',
+      'https://gd4.alicdn.com/imgextra/i1/539549300/TB2ffuDfk1M.eBjSZFFXXc3vVXa_!!539549300.jpg',
+      'https://gd1.alicdn.com/imgextra/i2/TB1t1l.X4HBK1JjSZFuYXFRSpXa_M2.SS2',
+      'https://gd1.alicdn.com/imgextra/i6/TB1CD89XZPEK1JjSZFEYXGA3XXa_M2.SS2',
+      'https://gd4.alicdn.com/imgextra/i4/539549300/TB2ayx6X36DK1JjSZJiXXalIVXa_!!539549300.jpg'
+    ]
+  }, {
+    icon: 'https://gd1.alicdn.com/imgextra/i2/749424884/TB2dMhNoHsTMeJjy1zcXXXAgXXa_!!749424884.jpg',
+    title: 'LAMPS 保暖和时髦的结合主义 一件可脱卸大围巾羽绒服什么做不到 💰1580',
+    pic: [
+      'https://gd1.alicdn.com/imgextra/i2/749424884/TB2dMhNoHsTMeJjy1zcXXXAgXXa_!!749424884.jpg',
+      'https://gd4.alicdn.com/imgextra/i4/749424884/TB26BhZj2NNTKJjSspeXXaSwpXa_!!749424884.jpg',
+      'https://gd3.alicdn.com/imgextra/i3/749424884/TB20qYtprsTMeJjSszhXXcGCFXa_!!749424884.jpg',
+      'https://gd3.alicdn.com/imgextra/i3/749424884/TB2k8tZj2NNTKJjSspeXXaSwpXa_!!749424884.jpg',
+      'https://gd3.alicdn.com/imgextra/i3/749424884/TB2vZKuiWagSKJjy0FgXXcRqFXa_!!749424884.jpg'
+    ]
+  }, {
+    icon: 'https://gd1.alicdn.com/imgextra/i2/749424884/TB2W06ihS7PL1JjSZFHXXcciXXa_!!749424884.jpg',
+    title: 'LAMPS 欧美时尚格子连衣裙 抹胸吊带裙不规则长裙子收腰显瘦A字裙 💰495',
+    pic: [
+      'https://gd1.alicdn.com/imgextra/i2/749424884/TB2W06ihS7PL1JjSZFHXXcciXXa_!!749424884.jpg',
+      'https://gd2.alicdn.com/imgextra/i2/749424884/TB29DHcXlcHL1JjSZJiXXcKcpXa_!!749424884.jpg',
+      'https://gd2.alicdn.com/imgextra/i2/749424884/TB2YL6aXekJL1JjSZFmXXcw0XXa_!!749424884.jpg',
+      'https://gd4.alicdn.com/imgextra/i4/749424884/TB2qWVraRUSMeJjy1zkXXaWmpXa_!!749424884.jpg',
+      'https://gd3.alicdn.com/imgextra/i3/749424884/TB2MOgmXBcHL1JjSZFBXXaiGXXa_!!749424884.jpg'
+    ]
+  }, {
+    icon: 'https://gd3.alicdn.com/imgextra/i1/749424884/TB2RdtCj2BNTKJjy1zdXXaScpXa_!!749424884.jpg',
+    title: 'LAMPS 阿水2017新款玫红色高领打底毛衣女套头针织衫长袖秋冬百搭  💰299',
+    pic: [
+      'https://gd3.alicdn.com/imgextra/i1/749424884/TB2RdtCj2BNTKJjy1zdXXaScpXa_!!749424884.jpg',
+      'https://gd4.alicdn.com/imgextra/i4/749424884/TB2PEn8kzuhSKJjSspdXXc11XXa_!!749424884.jpg',
+      'https://gd2.alicdn.com/imgextra/i2/749424884/TB1azr8kqagSKJjy0FaXXb0dpXa_!!0-item_pic.jpg',
+      'https://gd3.alicdn.com/imgextra/i1/749424884/TB2RdtCj2BNTKJjy1zdXXaScpXa_!!749424884.jpg',
+      'https://gd2.alicdn.com/imgextra/i2/749424884/TB2OhXLj2BNTKJjSszbXXaFrFXa_!!749424884.jpg'
+    ]
+  }, {
+    icon: 'https://gd1.alicdn.com/imgextra/i1/749424884/TB1gAXIcHsTMeJjSszgXXacpFXa_!!0-item_pic.jpg',
+    title: 'LAMPS复古卡其色裙裤女 秋冬显瘦性感开叉a字裙高腰系带短裤 💰369',
+    pic: [
+      'https://gd1.alicdn.com/imgextra/i1/749424884/TB1gAXIcHsTMeJjSszgXXacpFXa_!!0-item_pic.jpg',
+      'https://gd4.alicdn.com/imgextra/i4/749424884/TB2noFDcRUSMeJjSszbXXberFXa_!!749424884.jpg',
+      'https://gd2.alicdn.com/imgextra/i2/749424884/TB2QlRKcTZRMeJjSspkXXXGpXXa_!!749424884.jpg',
+      'https://gd3.alicdn.com/imgextra/i3/749424884/TB2sKxEcHsTMeJjy1zbXXchlVXa_!!749424884.jpg',
+      'https://gd2.alicdn.com/imgextra/i2/749424884/TB2xJFKeUQIL1JjSZFhXXaDZFXa_!!749424884.jpg'
+    ]
   }
 ]
