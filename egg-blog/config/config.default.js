3789bf9084e060914d38e0d1d0aacc1858076510
wanghuajian620 <154659041@qq.com>
2017-10-31 16:39:29 +0800

    blog 数据库 建成

---
+++ egg-blog/config/config.default.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+  exports.mysql = {
+    // 单数据库信息配置
+    client: {
+      // host
+      host: '127.0.0.1',
+      // 端口号
+      port: '3306',
+      // 用户名
+      user: 'root',
+      // 密码
+      password: '111111',
+      // 数据库名
+      database: 'test',
+    },
+  };
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1509435019629_8377';
+
+  // add your config here
+  config.middleware = [];
+  config.security = {
+    csrf: {
+      ignoreJSON: true,
+    },
+  };
+
+  return config;
+};
