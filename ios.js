7292d2fcb55d349a9506399569cc278eae348824
wanghuajian620 <154659041@qq.com>
2017-11-08 18:35:44 +0800

    组件练习2

--- ios.js
+++ ios.js
@@ -40,6 +40,58 @@ ReactDOM.render(
   document.getElementById('root')
 );// 3
 
+function App(props) {
+  return <h1>Hello, {props.name}</h1>
+} 
 
+const element = <App name="xiaoming" />
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+)
+
+
+
+
+function Child(props) {
+  return <h1>nihao, {props.name}</h1>
+}
+
+function Parent() {
+  return (
+    <div>
+      <Child name="Sara" /> 
+      <Child name="Mary" />
+    </div>   // 在 组件嵌套中，父组件可以 render 子组件 很多次
+  )
+}
+ReactDOM.render(
+  <Parent />, // 组件的表示形式
+  document.getElementById('root') // root 是根节点， 这里是用Parent代替 root
+)
+
+
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
 
 
