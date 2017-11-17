35cc45ef5d6a58bd2ac001e0d028cf434ba72059
wanghuajian620 <154659041@qq.com>
2017-11-17 20:26:49 +0800

    新增 路由配置return

--- ios.js
+++ ios.js
@@ -121,3 +121,44 @@ ReactDOM.render (
   document.getElementById('root')
 );
 
+
+
+// 路由配置的return
+const App = React.createClass({
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+        <ul>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/inbox">Inbox</Link></li>
+        </ul>
+        {this.props.children}
+      </div>
+    )
+  }
+})
+
+const About = React.createClass({
+  render() {
+    return <h3>About</h3>
+  }
+})
+
+const Inbox = React.createClass({
+  render() {
+    return (
+      <div>
+        <h2>Inbox</h2>
+        {this.props.children || "Welcome to your Inbox"}
+      </div>
+    )
+  }
+})
+
+const Message = React.createClass({
+  render() {
+    return <h3>Message {this.props.params.id}</h3>
+  }
+})
+
