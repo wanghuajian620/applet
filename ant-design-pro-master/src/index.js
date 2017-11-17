ff421874416410e94c04e0b0c50d9d8eac309e52
wanghuajian620 <154659041@qq.com>
2017-11-17 20:38:01 +0800

    蚂蚁

---
+++ ant-design-pro-master/src/index.js
@@ -0,0 +1,23 @@
+import dva from 'dva';
+import 'moment/locale/zh-cn';
+import './polyfill';
+import './g2';
+// import { browserHistory } from 'dva/router';
+import './index.less';
+
+// 1. Initialize
+const app = dva({
+  // history: browserHistory,
+});
+
+// 2. Plugins
+// app.use({});
+
+// 3. Register global model
+app.model(require('./models/global'));
+
+// 4. Router
+app.router(require('./router'));
+
+// 5. Start
+app.start('#root');
