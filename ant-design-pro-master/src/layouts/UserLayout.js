5cee5aeff0d6b017f5a990812fe24c28a745305a
wanghuajian620 <154659041@qq.com>
2017-11-18 20:12:50 +0800

    用户登录界面的更改

--- ant-design-pro-master/src/layouts/UserLayout.js
+++ ant-design-pro-master/src/layouts/UserLayout.js
@@ -17,7 +17,7 @@ const links = [{
   href: '',
 }];
 
-const copyright = <div>Copyright <Icon type="copyright" /> 2017 蚂蚁金服体验技术部出品</div>;
+const copyright = <div>Copyright <Icon type="copyright" /> 2017 王华建的个人博客</div>;
 
 class UserLayout extends React.PureComponent {
   static childContextTypes = {
